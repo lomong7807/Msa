dec4e416ac226c596cd9613843f45e21a4790116
lomong7807 <lomong7807@gmail.com>
2024-09-19 00:27:02 +0900

    MSA practice

---
+++ user-service/src/main/java/com/example/userservice/dto/UserDto.java
@@ -0,0 +1,13 @@
+package com.example.userservice.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+
+    private String email;
+    private String name;
+    private String pwd;
+    private String userId;
+
+}
