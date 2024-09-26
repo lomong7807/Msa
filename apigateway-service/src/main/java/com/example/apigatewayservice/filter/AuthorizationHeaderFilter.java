557190002d0c9175a992dfef0203cdefb47c4edf
lomong7807 <lomong7807@gmail.com>
2024-09-26 23:55:11 +0900

    rabbitMQ

--- apigateway-service/src/main/java/com/example/apigatewayservice/filter/AuthorizationHeaderFilter.java
+++ apigateway-service/src/main/java/com/example/apigatewayservice/filter/AuthorizationHeaderFilter.java
@@ -68,11 +68,13 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
         String subject = null;
 
         try{
+            String exp = env.getProperty("token.expiration_time");
+            String secret = env.getProperty("token.secret");
+
             subject = Jwts.parser().setSigningKey(env.getProperty("token.secret"))
                     .parseClaimsJws(jwt).getBody()
                     .getSubject();
-            String exp = env.getProperty("token.expiration_time");
-            String secret = env.getProperty("token.secret");
+
         }catch (Exception e) {
             returnValue = false;
         }
